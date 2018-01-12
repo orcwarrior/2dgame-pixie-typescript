7dee0d690f35d4fab82ef637f57bbb28fc33ef47
orcwarrior <orcwarrior@o2.pl>
2018-01-12 01:42:07 +0100

    GameManager: MOBFactory, updateableObjects[] (+needed methods); dirty testing MOB and InputManager

--- src/scripts/engine/GameManager.ts
+++ src/scripts/engine/GameManager.ts
@@ -1,20 +1,26 @@
 import * as PIXI from 'pixi.js';
 import {InputManager} from './InputManager';
+import {Updateable} from './interfaces/Updateable';
+import {moveableObjectFactory} from '../factories/MoveableObjectFactory';
+import {Key} from 'ts-keycode-enum';
 
 let goombaUrl = require('file-loader!../../res/sprites/goomba.png');
 
 export class GameManager {
-    private app: any;
     private gameWrapper: HTMLElement | null;
-    private objects: PIXI.Container;
     private renderer: PIXI.WebGLRenderer | PIXI.CanvasRenderer;
     private ticker: PIXI.ticker.Ticker;
     // Managers:
     private inputMgr: InputManager;
+    // Factories:
+    private moveableObjsFactory: Function;
+    // Objects:
+    private objects: PIXI.Container;
+    private updateableObjects: Array<Updateable> = [];
 
     constructor(wrapperId: string) {
         let app = this.renderer = PIXI.autoDetectRenderer({width: 800, height: 600});
-        this.objects = new PIXI.Container();
+        // this.objects = new PIXI.Container();
         this.inputMgr = new InputManager();
         let updateTicker = this.ticker = PIXI.ticker.shared;
         updateTicker.add(this.update, this);
@@ -23,23 +29,43 @@ export class GameManager {
         if (this.gameWrapper) {
             this.gameWrapper.appendChild(this.renderer.view);
         }
+        this.moveableObjsFactory = moveableObjectFactory(this);
+        this.objects = new PIXI.Container();
 
-        const goomba = PIXI.Sprite.fromImage(goombaUrl);
-        goomba.anchor.set(0.5);
-        goomba.x = app.screen.width / 2;
-        goomba.y = app.screen.height / 2;
-        goomba.width = 40;
-        goomba.height = 40;
 
-        this.objects.addChild(goomba);
-        let goombaTicker = new PIXI.ticker.Ticker();
-        goombaTicker.add((deltaTime) => {
-            goomba.x += 1;
-        });
-        goombaTicker.start();
+
+        const goombaSprite = PIXI.Sprite.fromImage(goombaUrl);
+        goombaSprite.anchor.set(0.5);
+        goombaSprite.x = app.screen.width / 2;
+        goombaSprite.y = app.screen.height / 2;
+        goombaSprite.width = 40;
+        goombaSprite.height = 40;
+
+        const goomba = this.moveableObjsFactory(goombaSprite);
+        this.inputMgr.addKeyEventListener('keydown', Key.UpArrow, () => goomba.my = -1);
+        this.inputMgr.addKeyEventListener('keydown', Key.DownArrow, () => goomba.my = 1);
+        this.inputMgr.addKeyEventListener('keydown', Key.LeftArrow, () => goomba.mx = -1);
+        this.inputMgr.addKeyEventListener('keydown', Key.RightArrow, () => goomba.mx = 1);
+        this.inputMgr.addKeyEventListener('keyup', Key.UpArrow, () => goomba.my = 0);
+        this.inputMgr.addKeyEventListener('keyup', Key.DownArrow, () => goomba.my = 0);
+        this.inputMgr.addKeyEventListener('keyup', Key.LeftArrow, () => goomba.mx = 0);
+        this.inputMgr.addKeyEventListener('keyup', Key.RightArrow, () => goomba.mx = 0);
+
+    }
+    public getInputManager() {
+        return this.inputMgr;
     }
 
-    private update() {
+    private update(delta: number) {
+        this.updateableObjects.forEach((mob) => mob.update(delta));
         this.renderer.render(this.objects);
     }
+
+    public addUpdateableObject(obj: Updateable) {
+        this.updateableObjects.push(obj);
+    }
+    public addRenderedSprite(sprite: PIXI.Sprite) {
+        this.objects.addChild(sprite);
+    }
+
 }
