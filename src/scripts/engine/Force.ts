ae2e1ea214ad9b40e0a2d9c9a4de8095c19fc4fd
orcwarrior <orcwarrior@o2.pl>
2018-01-16 19:30:26 +0100

    Force: Now accepts both dir parameter (converts it to vector) and vector for force direction; added some static easing functions.

--- src/scripts/engine/Force.ts
+++ src/scripts/engine/Force.ts
@@ -1,3 +1,5 @@
+import {pointMul} from './utils/utils';
+
 const ForceDir = {
     UP: (f: number) => new PIXI.Point(0, -f),
     RIGHT: (f: number) => new PIXI.Point(f, 0),
@@ -13,22 +15,26 @@ export enum Direction {
 }
 
 export class Force extends PIXI.utils.EventEmitter {
-    public static constant = function () {
-        return 1;
-    };
-    public static easeLinear = function (p) {
-        return p;
-    };
 
     private durationMS: number;
     private _initialForce: number;
     private easeFunction: Function;
-    private direction: Direction;
     private _initTimestamp: Date;
+    private directionVector: PIXI.Point;
 
-    constructor(dir: Direction, initialForce: number, durationMS: number, easeFunction?: Function) {
+    /* statics */
+    public static constant = function () {
+        return 1;
+    };
+    public static easeLinear = function (p: number) {
+        return p;
+    };
+
+    constructor(dir: Direction | PIXI.Point, initialForce: number, durationMS: number,
+                easeFunction: Function = Force.easeLinear) {
         super();
-        this.direction = dir;
+
+        this.directionVector = this.getDirectionVector(dir);
         this._initialForce = initialForce;
         this.durationMS = durationMS;
         this.easeFunction = easeFunction || Force.easeLinear;
@@ -39,13 +45,20 @@ export class Force extends PIXI.utils.EventEmitter {
     public getCurrentVector() {
         let progress = this.getForceProgress();
         let force = this._initialForce * this.easeFunction(1 - progress);
-        let forceVector = ForceDir[this.direction](force);
+        let forceVector = pointMul(this.directionVector, force);
         if (progress >= 1) { this.emit('end', progress, forceVector); }
         return forceVector;
 
     }
-
-    private getForceProgress() {
+    private getDirectionVector(dir: Direction | PIXI.Point): PIXI.Point {
+        let asDirection = (<Direction>dir);
+        if (Direction[asDirection]) {
+            return ForceDir[asDirection](1);
+        } else {
+            return <PIXI.Point>dir;
+        }
+    }
+    private getForceProgress(): number {
         let curTimestamp: Date = new Date();
         let diff = (curTimestamp.getTime() - this._initTimestamp.getTime());
         return diff / this.durationMS;
