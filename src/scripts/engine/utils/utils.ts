a8981e23624f3ae0b9b72503113af2777d4fe675
orcwarrior <orcwarrior@o2.pl>
2018-01-17 15:46:19 +0100

    utils: removed nolonger used points* method

--- src/scripts/engine/utils/utils.ts
+++ src/scripts/engine/utils/utils.ts
@@ -3,11 +3,3 @@ import AnimatedSprite = PIXI.extras.AnimatedSprite;
 export interface AnimsMap {
     [key: string]: AnimatedSprite;
 }
-
-export function pointsAdd(a: PIXI.Point, b: PIXI.Point) {
-    return new PIXI.Point(a.x + b.x, a.y + b.y);
-}
-
-export function pointMul(p: PIXI.Point, m: number) {
-    return new PIXI.Point(p.x * m, p.y * m);
-}
