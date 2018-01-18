a5141a350d88f73a5c611a3fdb5d100f6595e8c3
orcwarrior <orcwarrior@o2.pl>
2018-01-18 04:23:26 +0100

    GenericVisualComponent with VisualComponentConfig which now can pass children Display objects.

--- src/scripts/engine/gameObject/components/GenericVisualComponent.ts
+++ src/scripts/engine/gameObject/components/GenericVisualComponent.ts
@@ -1,14 +1,25 @@
 import {VisualComponent} from './abstract/VisualComponent';
 import {GameManager} from '../../GameManager';
+import DisplayObject = PIXI.DisplayObject;
 
+export interface VisualComponentConfig {
+    x: number;
+    y: number;
+    childs?: PIXI.DisplayObject[];
+}
 export class GenericVisualComponent extends VisualComponent {
-    constructor(container?: PIXI.Container, config = {x: 0, y: 0}, parentContainer?: PIXI.Container) {
+    constructor(config: VisualComponentConfig = {x: 0, y: 0}, container?: PIXI.Container,
+                parentContainer?: PIXI.Container, ) {
         super(container);
         this.container.x = config.x;
         this.container.y = config.y;
         this.container.setParent(parentContainer || GameManager.instance.getRootContainer());
+        if (config.childs) {
+            config.childs.forEach((child: DisplayObject) => this.container.addChild(child));
+        }
         GameManager.instance.addRenderableObject(this);
     }
+
     public render(renderContext: PIXI.WebGLRenderer | PIXI.CanvasRenderer): void {
         renderContext.render(this.container);
     }
