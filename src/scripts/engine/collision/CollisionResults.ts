1f5375cfbaf14df20ac23ff4d399c53bb9d5e5a3
orcwarrior <orcwarrior@o2.pl>
2018-01-18 02:13:58 +0100

    CollisionResults: class to present sides on which collision occured

---
+++ src/scripts/engine/collision/CollisionResults.ts
@@ -0,0 +1,39 @@
+import {Vector} from '../utils/Vector';
+import {Direction, directionEnumKeys} from '../Force';
+
+// NOTE: CollisionVector is the way to note
+// about collision (or no collision) in one of four directions
+// example x-axis:
+// -1: collision from left
+//  0: no collision
+//  1: collision from right
+// If you dont get it simpy use filterVelocityVector method.
+// NOTE2: In more platformer-like enviroment simple vector could be
+// not enough
+export class CollisionResults {
+    protected collisionSides: {[side: string]: boolean};
+    constructor() {
+        this.collisionSides = {};
+    }
+    public collideSide(side: Direction) {
+        this.collisionSides[side] = true;
+    }
+    public isSideCollided(side: Direction) {
+        return this.collisionSides[side];
+    }
+    public join(collRes: CollisionResults): void {
+        directionEnumKeys.forEach((dirName) =>
+            this.collisionSides[dirName] = this.collisionSides[dirName] || collRes.isSideCollided(dirName)
+        );
+    }
+
+    public filterVelocityVector(v: Vector) {
+        let out = new Vector(v.x, v.y);
+        let signs = v.signVector();
+        if (signs.x === -1 && this.collisionSides[Direction.LEFT]) { out.x = 0; }
+        if (signs.x === 1 && this.collisionSides[Direction.RIGHT]) { out.x = 0; }
+        if (signs.y === -1 && this.collisionSides[Direction.UP]) { out.y = 0; }
+        if (signs.y === 1 && this.collisionSides[Direction.DOWN]) { out.y = 0; }
+        return out;
+    }
+}
