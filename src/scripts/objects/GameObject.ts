9a43eaffaf470d9d32d2e8759a686b68dddd30c7
orcwarrior <orcwarrior@o2.pl>
2018-01-18 02:09:26 +0100

    GameObjects: components moved from player to GameObjects

--- src/scripts/objects/GameObject.ts
+++ src/scripts/objects/GameObject.ts
@@ -1,7 +1,19 @@
 import EventEmitter = PIXI.utils.EventEmitter;
+import {InputComponent} from '../engine/gameObjectComponents/InputComponent';
+import {AnimsComponent} from '../engine/gameObjectComponents/AnimsComponent';
+import {MoveableComponent} from '../engine/gameObjectComponents/MoveableComponent';
+import {VisualComponent} from '../engine/gameObjectComponents/VisualComponent';
+import {CollisionComponent} from '../engine/gameObjectComponents/CollisionComponent';
 
 export abstract class GameObject extends EventEmitter {
+    protected inputComponent: InputComponent;
+    protected aniComponent: AnimsComponent;
+    protected moveableComponent: MoveableComponent;
+    protected visualComponent: VisualComponent;
+    protected collisionComponent: CollisionComponent;
+
     constructor() {
         super();
     }
+    public abstract update(delta: number): void;
 }
