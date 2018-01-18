56af196e06b2c7e2076f0cd8c48e7209d30197c5
orcwarrior <orcwarrior@o2.pl>
2018-01-18 16:08:31 +0100

    StaticObject: collision is now optional

--- src/scripts/engine/gameObject/objects/StaticObject.ts
+++ src/scripts/engine/gameObject/objects/StaticObject.ts
@@ -3,12 +3,14 @@ import {VisualComponent} from '../components/abstract/VisualComponent';
 import {PassiveCollisionComponent} from '../components/PassiveCollisionComponent';
 
 export class StaticObject extends GameObject {
-    constructor(visual: VisualComponent) {
+    constructor(visual: VisualComponent, collision: boolean = true) {
         super();
         this.visualComponent = visual;
-        let containerBoundingRect = visual.getContainer().getBounds.bind(visual.getContainer());
+        let containerBoundingRect = visual.getVisual().getBounds.bind(visual.getVisual());
 
-        this.collisionComponent = new PassiveCollisionComponent(this, containerBoundingRect);
+        if (collision) {
+            this.collisionComponent = new PassiveCollisionComponent(this, containerBoundingRect);
+        }
 
     }
     public update(delta: number): void { return; }
