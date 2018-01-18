e36e925915d76016987ccf3a6815fbc79ffb43fc
orcwarrior <orcwarrior@o2.pl>
2018-01-18 02:10:34 +0100

    utils: IntersectRect function

--- src/scripts/engine/utils/utils.ts
+++ src/scripts/engine/utils/utils.ts
@@ -3,3 +3,16 @@ import AnimatedSprite = PIXI.extras.AnimatedSprite;
 export interface AnimsMap {
     [key: string]: AnimatedSprite;
 }
+
+export function IntersectRect(a: PIXI.Rectangle, b: PIXI.Rectangle): PIXI.Rectangle {
+    let xStart = Math.max(a.x, b.x);
+    let yStart = Math.max(a.x, b.y);
+    let xEnd = Math.min(a.x + a.width, b.x + b.width);
+    let yEnd = Math.min(a.y + a.height, b.y + b.height);
+    if (xEnd >= xStart && yEnd >= yStart) {
+        return new PIXI.Rectangle(xStart, yStart, xEnd - xStart, yEnd - yStart);
+    } else {
+        return PIXI.Rectangle.EMPTY;
+    }
+}
+
