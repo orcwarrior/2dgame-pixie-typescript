40abb0f69e3ed59436cacc01a68147866670112a
orcwarrior <orcwarrior@o2.pl>
2018-01-19 06:05:03 +0100

    VisualComponent: destroy

--- src/scripts/engine/gameObject/components/abstract/VisualComponent.ts
+++ src/scripts/engine/gameObject/components/abstract/VisualComponent.ts
@@ -1,4 +1,5 @@
 import {Renderable} from '../../../interfaces/Renderable';
+import {GameManager} from '../../../GameManager';
 
 export abstract class VisualComponent implements Renderable {
     protected visualObj: PIXI.DisplayObject;
@@ -6,8 +7,16 @@ export abstract class VisualComponent implements Renderable {
     constructor(vObj: PIXI.DisplayObject = new PIXI.Container()) {
         this.visualObj = vObj;
     }
-    public getVisual(): PIXI.DisplayObject {
-        return this.visualObj;
-    }
     public abstract render(renderContext: PIXI.WebGLRenderer | PIXI.CanvasRenderer): void;
+
+    public getVisual(): PIXI.DisplayObject { return this.visualObj; }
+
+    public destroy() {
+        GameManager.instance.scene.removeVisual(this);
+        // if ((<PIXI.Container>this.visualObj).children) {
+        //     (<PIXI.Container>this.visualObj).destroy({children: true});
+        // } else {
+        //     this.visualObj.destroy();
+        // }
+    }
 }
