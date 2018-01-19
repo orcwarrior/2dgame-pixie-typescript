7a3b6de4d0f2d398d7fc44069c0a0c1d656e99a6
orcwarrior <orcwarrior@o2.pl>
2018-01-19 16:18:54 +0100

    added scene foreground barriers

--- src/scripts/engine/scene/firstSceneFactory.ts
+++ src/scripts/engine/scene/firstSceneFactory.ts
@@ -1,14 +1,13 @@
 import {Scene} from './Scene';
 import {StaticObject} from '../gameObject/objects/StaticObject';
 import {GenericVisualComponent} from '../gameObject/components/GenericVisualComponent';
-import {Player} from '../gameObject/player/Player';
-import {foodFactory, foodFactoryResult, getFoodOriginLocations} from '../factories/foodFactory';
 import {orgCoordToScaled} from '../utils/coordScalar';
-import {delayedPromise} from '../utils/delayedPromise';
 import {GameManager} from '../GameManager';
+import BaseTexture = PIXI.BaseTexture;
 
 let bgRes = require('file-loader!res/sprites/bg.png');
 let goombaRes = require('file-loader!res/sprites/goomba.png');
+let barrierRes = require('file-loader!res/sprites/derbis/barrier.png');
 
 
 export function firstSceneFactory(): Scene {
@@ -28,14 +27,30 @@ export function firstSceneFactory(): Scene {
     let bridgeVis = new GenericVisualComponent(bridgeGFX);
     let bridge = new StaticObject(bridgeVis);
     scene.addBackground(bridgeVis);
+
     // TODO: Way of creating & configuring sprites is big no-no
-    // extract to some util function
+    // extract to some util/factory function
+    // (factory with some mutation function after onLoad would be nice -> for screen scaling propouses etc)
     const goombaSprite = PIXI.Sprite.fromImage(goombaRes);
     goombaSprite.width = 50;
     goombaSprite.height = 50;
     let goombaVis = new GenericVisualComponent(goombaSprite, {x: sX(20), y: sY(230)});
     let goomba = new StaticObject(goombaVis);
     scene.addObject(goombaVis);
+
+
+    [sX(2), sX(130), sX(270), sX(400)].forEach((x) => {
+        const barrierTex = PIXI.Texture.fromImage(barrierRes, undefined, PIXI.SCALE_MODES.NEAREST);
+        barrierTex.baseTexture.once('loaded', (baseTex: BaseTexture) => {
+            let barrierSprite = PIXI.Sprite.from(baseTex);
+            barrierSprite.width *= sX(1);
+            barrierSprite.height *= sY(1);
+            let barrierVis = new GenericVisualComponent(barrierSprite, {x: x, y: sY(224)});
+            let barrier = new StaticObject(barrierVis, false);
+            scene.addForeground(barrierVis);
+        });
+
+    })
 
 
     return scene;
